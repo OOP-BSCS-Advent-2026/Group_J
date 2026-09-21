c9e8811ee6fa4c07707f3b47b5d3f4eccf9ad4d5
isaaclule6 <lulemeshaisaac@gmail.com>
2026-09-21 08:58:17 +0300

    Create FlatDiscountItem.java
    Worked on the flatdiscountitem to the program. updates the maini and it operates when quantity purchases meets the conditions and updates the main

---
+++ FlatDiscountItem.java
@@ -0,0 +1,23 @@
+// The Inheritance structure
+public class FlatDiscountItem extends Item {
+    // Encapsulation
+    private int threshold;
+    private double flatAmount;
+
+    public FlatDiscountItem(String name, double price,
+                            int threshold, double flatAmount) {
+        super(name, price);
+        this.threshold = threshold;
+        this.flatAmount = flatAmount;
+    }
+
+    //Overriding
+    @Override
+    public double calculateTotal(int quantity) {
+        double subtotal = super.calculateTotal(quantity);
+        if (quantity >= threshold) {
+            subtotal = Math.max(0, subtotal - flatAmount);
+        }
+        return subtotal;
+    }
+}
