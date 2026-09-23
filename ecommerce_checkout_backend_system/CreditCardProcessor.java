914c4ade34f7054da8798efb3e3d6f38c21aae0f
ArthurKavuma <arthurkavuma1@gmail.com>
2026-09-23 22:27:20 +0300

    Update CreditCardProcessor.java

--- ecommerce_checkout_backend_system/CreditCardProcessor.java
+++ ecommerce_checkout_backend_system/CreditCardProcessor.java
@@ -1,7 +1,7 @@
 public class CreditCardProcessor extends PaymentProcessor{
 
   @Override
-  public void ProcessPayment(double amount){
+  public void processPayment(double amount){
     System.out.println("Processing UGX" + amount + " using credit card");
   }
 }  
